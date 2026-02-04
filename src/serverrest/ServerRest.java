a3c66d97e25bb6c25fe4780f2afd7c3c5f1e60b2
Stefano Del Furia <stefano.delfuria@franchettisalviani.net>
2026-02-04 13:33:29 +0100

    Refactoring per separazione App.java da ServerRest

--- src/serverrest/ServerRest.java
+++ src/serverrest/ServerRest.java
@@ -6,6 +6,7 @@
 package serverrest;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.IOException;
@@ -16,27 +17,18 @@ import java.util.Map;
 
 
 /**
- *
+ * Server REST per la calcolatrice
+ * 
  * @author delfo
  */
 public class ServerRest {
 
     /**
-     * @param args the command line arguments
+     * Avvia il server REST sulla porta specificata
+     * 
+     * @param porta la porta su cui avviare il server
      */
-    
-    public static void main(String[] args) {
-        
-        // Configurazione porta (default 8080)
-        int porta = 8080;
-        if (args.length > 0) {
-            try {
-                porta = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Porta non valida, uso porta default 8080");
-            }
-        }
-        
+    public static void avviaServer(int porta) {
         try {
             // Crea il server sulla porta specificata
             HttpServer server = HttpServer.create(new InetSocketAddress(porta), 0);
@@ -46,34 +38,7 @@ public class ServerRest {
             server.createContext("/api/calcola/get", new GetHandler());
             
             // Endpoint di benvenuto
-            server.createContext("/", exchange -> {
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                
-                Map info = new HashMap<>();
-                info.put("messaggio", "Benvenuto alla Calcolatrice REST API");
-                info.put("versione", "2.0.0");
-                info.put("tecnologia", "Java + GSON");
-                
-                Map endpoints = new HashMap<>();
-                endpoints.put("POST", "/api/calcola/post");
-                endpoints.put("GET", "/api/calcola/get?operando1=X&operando2=Y&operatore=OP");
-                info.put("endpoints", endpoints);
-                
-                Map operatori = new HashMap<>();
-                operatori.put("somma", "SOMMA o +");
-                operatori.put("sottrazione", "SOTTRAZIONE o -");
-                operatori.put("moltiplicazione", "MOLTIPLICAZIONE o * o X");
-                operatori.put("divisione", "DIVISIONE o /");
-                info.put("operatori_supportati", operatori);
-                
-                String jsonRisposta = gson.toJson(info);
-                
-                exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
-                byte[] bytes = jsonRisposta.getBytes();
-                exchange.sendResponseHeaders(200, bytes.length);
-                exchange.getResponseBody().write(bytes);
-                exchange.getResponseBody().close();
-            });
+            server.createContext("/", ServerRest::gestisciBenvenuto);
             
             // Avvia il server
             server.setExecutor(null); // Usa il default executor
@@ -100,5 +65,40 @@ public class ServerRest {
             System.err.println("Errore nell'avvio del server: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+    
+    /**
+     * Gestisce l'endpoint di benvenuto che fornisce informazioni sull'API
+     * 
+     * @param exchange l'oggetto HttpExchange per gestire la richiesta/risposta
+     * @throws IOException in caso di errori durante la comunicazione
+     */
+    private static void gestisciBenvenuto(HttpExchange exchange) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        
+        Map info = new HashMap<>();
+        info.put("messaggio", "Benvenuto alla Calcolatrice REST API");
+        info.put("versione", "2.0.0");
+        info.put("tecnologia", "Java + GSON");
+        
+        Map endpoints = new HashMap<>();
+        endpoints.put("POST", "/api/calcola/post");
+        endpoints.put("GET", "/api/calcola/get?operando1=X&operando2=Y&operatore=OP");
+        info.put("endpoints", endpoints);
+        
+        Map operatori = new HashMap<>();
+        operatori.put("somma", "SOMMA o +");
+        operatori.put("sottrazione", "SOTTRAZIONE o -");
+        operatori.put("moltiplicazione", "MOLTIPLICAZIONE o * o X");
+        operatori.put("divisione", "DIVISIONE o /");
+        info.put("operatori_supportati", operatori);
+        
+        String jsonRisposta = gson.toJson(info);
+        
+        exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+        byte[] bytes = jsonRisposta.getBytes();
+        exchange.sendResponseHeaders(200, bytes.length);
+        exchange.getResponseBody().write(bytes);
+        exchange.getResponseBody().close();
     }
 }
